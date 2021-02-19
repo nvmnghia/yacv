5eae2bd3d6261b93cbf4f574c47bbaa06b6047b2
nvmnghia <nvmnghia@gmail.com>
2021-02-19 07:24:00 +0700

    wip: use `SearchViewModel` to handle search

---
+++ app/src/main/java/com/uet/nvmnghia/yacv/ui/search/SearchViewModel.kt
@@ -0,0 +1,13 @@
+package com.uet.nvmnghia.yacv.ui.search
+
+import androidx.hilt.lifecycle.ViewModelInject
+import com.uet.nvmnghia.yacv.model.search.MetadataSearchHandler
+
+
+class SearchViewModel @ViewModelInject constructor(
+    private val metadataSearchHandler: MetadataSearchHandler
+) {
+
+
+
+}
